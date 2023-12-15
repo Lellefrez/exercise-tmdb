0d1ef6ae62a9a1248a7439341d261915b666e531
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-12-15 20:23:24 +0100

    feat| non funziona API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import "./App.scss";
 const apiKey = import.meta.env.VITE_API_KEY;
 function App() {
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`).then(
+      (response) => {
+        console.log(response);
+      }
+    );
+  }, []);
   return <></>;
 }
 
