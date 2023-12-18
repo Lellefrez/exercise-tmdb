bb5ac6c3d175360ef4008d0b894295d4147cbac7
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-12-18 22:27:15 +0100

    feat | Creato-Select

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -3,9 +3,13 @@ import { useState } from "react";
 export default ({ onSearch }) => {
   const [value, setvalue] = useState("");
 
-  
   return (
     <div className="search-bar">
+      <select name="generes" id="generes">
+          <option value="">Movie</option>
+          <option value="">Tv Series</option>
+          <option value="">Person</option>
+      </select>
       <input
         type="text"
         placeholder="Cerca film..."
